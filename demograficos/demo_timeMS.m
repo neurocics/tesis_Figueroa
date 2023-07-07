59e9ede9fd89ef6e03407621869dcf1ee94caf4c
amfigueroaudd <91983264+amfigueroaudd@users.noreply.github.com>
2023-07-07 16:02:04 -0400

    update

---
+++ demograficos/demo_timeMS.m
@@ -0,0 +1,61 @@
+%% cálculo de tiempo de evolución MS
+
+% Datos de timeMS
+timeMS = [14, 7, 4, 8, 7, 8, 12, 9, 20, 9, 11, 9, 6, 7, 4, 0.9, 7, 3];
+
+% Cálculo del promedio timeMS
+promedio = mean(timeMS);
+
+% Cálculo de la desviación estándar timeMS
+desviacion = std(timeMS);
+
+% Mostrar los resultados
+fprintf('Promedio: %.2f\n', promedio);
+fprintf('Desviación estándar: %.2f\n', desviacion);
+
+%% cálculo de edad MS
+
+% Datos de edades MS
+edadesMS = [53.9, 49.3, 32.6, 44.8, 31.3, 39.8, 46.3, 60.8, 43.9, 43.8, 43.0, 38.6, 45.2, 35.3, 27.9, 36.8, 32.7, 47.6, 32.3];
+
+% Cálculo del promedio edades MS
+promedio = mean(edadesMS);
+
+% Cálculo de la desviación estándar timeMS
+desviacion = std(edadesMS);
+
+% Mostrar los resultados
+fprintf('Promedio: %.2f\n', promedio);
+fprintf('Desviación estándar: %.2f\n', desviacion);
+
+
+%% %% cálculo de edad controles
+
+% Datos de edades controles
+edadesCON = [30.5, 34.6, 28.9, 34.0, 31.6, 34.8, 39.2, 51.9, 44.7, 43.5, 46.7, 36.8, 53.9, 38.7];
+
+% Cálculo del promedio edades controles
+promedio = mean(edadesCON);
+
+% Cálculo de la desviación estándar timeMS
+desviacion = std(edadesCON);
+
+% Mostrar los resultados
+fprintf('Promedio: %.2f\n', promedio);
+fprintf('Desviación estándar: %.2f\n', desviacion);
+
+%% %% cálculo años escolaridad MS
+
+% Datos de escolaridad MS
+    escolaMS = [18, 18, 14, 16, 16, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18]
+    
+    % Cálculo del promedio escolaridad MS
+promedio = mean(escolaMS);
+
+% Cálculo de la desviación estándar timeMS
+desviacion = std(escolaMS);
+
+% Mostrar los resultados
+fprintf('Promedio: %.2f\n', promedio);
+fprintf('Desviación estándar: %.2f\n', desviacion);
+    
--- demograficos/yearsms.m
+++ demograficos/yearsms.m
@@ -1,8 +1,8 @@
- % Fechas de diagnóstico MS
+ % Calcular tiempo  de diagnóstico MS al día del experimento (EEG)
+ 
 fechasDiagnostico = ["2019-01-01", "2015-01-01", "2011-01-01", "2013-01-01", "2003-01-01", "2013-01-01", "2012-01-01", "2013-01-01", "2016-01-01", "2018-01-01", "2022-01-01", "2016-01-01"]; % Año diagnóstico
 
 % Obtener la fecha actual
-
 
 fechaActual = datetime('2023-01-01');
 
@@ -18,16 +18,71 @@ end
 % save en carpeta demografica 
 save '/Volumes/Alehermosa/TESIS/demo'
 
+%% Calcular edad de participantes al día del experimento (EEG)
+
+
+SU= {{
+'CFOI_23101987' % 'RL WM GN'     %
+'CAMS_24101973' % 'RL WM GN'  %
+'FLFS_08081990' % 'RL GN WM'  % 
+'GAMY_03071978' % RL GN WM'  % 
+'K_BA_22111991' % RL GN WM % 
+'KSDG_20051983' % RL GN WM %  
+'MMPM_06011977' % 'RL GN WM' 
+'S_WL_25051979' % 'RL GN WM'
+'CAPG_27061969' % 'RL GN WM'
+'MJDD_01101984'
+'SFVB_26031991'
+'N_LS_15111975'
+'FSDT_10101990'
+'DITR_30081986'
+'J_AA_31071995'
+'MALV_18031978'
+'TPAO_16051980'
+'PMRF_18051979'
+'MPRM_13061962'
+
+}',{% 'RL GN WM' 
+'CEAS_23071992' % 'RL GN WM' 
+'CAHG_27061988' % 'RL GN WM' 
+'YDCL_11021994' % 'RL GN WM' 
+'B_FE_24011989' % 'RL GN WM' 
+'MEFR_14061991' % 'RL GN WM' 
+'SNCS_22121989' % 'RL GN WM' 
+'MAAO_15081988' % 'RL GN WM'  
+'GRCN_21111979' % 'RL GN WM'  
+'KXMS_13061971' % 'RL GN WM' 
+'CFPL_08021984' % 'RL GN WM' 
+'PIHV_23091978'
+'JFFA_04101976'
+'PGTR_30081986'
+'AAAS_13071969'
+'MJMM_18101984'
+'PSAV_10041994'
+}'} ;
+
+
+GRS={'pacientes','controles'};
+
+OVERWRITE=false;
+
 %%
+% Abrir el archivo en modo escritura (si el archivo no existe, se creará)
+archivo = fopen('/Volumes/Alehermosa/TESIS/datos_demograficos.txt', 'w');
 
+% Verificar si el archivo se abrió correctamente
+if archivo == -1
+    error('No se pudo abrir el archivo.');
+end
 
-SU={'B_FE_24011989' % 'RL GN WM' 
-} ;
-
-GR={'controles'};
-
-
-D = ls ( ['/Volumes/Alehermosa/TESIS/' GR{1} '/' SU{1} '/EEG/*GN.eeg'])
+for nG = 1:2
+    GR= GRS{nG};
+    SU_g = SU{nG};
+for nS = 1:numel(SU_g)
+    % for nS = Su_g
+    
+D = ls (['/Volumes/Alehermosa/TESIS/' GR '/' SU_g{nS}  '/EEG/*GN.eeg']);
+%D = ls (['/Volumes/Alehermosa/TESIS/' GR '/' nS{1}  '/EEG/*GN.eeg']);
 
 fechaEEG = datetime([ D(end-11:end-8)  '-' D(end-13:end-12) '-'  D(end-15:end-14)]);
 
@@ -37,5 +92,15 @@ fechaNAC = datetime([ D(end-20:end-17)  '-' D(end-22:end-21) '-'  D(end-24:end-2
 
 edad = years(fechaEEG - datetime(fechaNAC, 'InputFormat', 'yyyy-MM-dd'));
 
-fprintf('Tu edad en el día del experimento es: %.2f años\n', edad);
+% calcular edad de participantes en el momento del EEG MS
+fprintf('El sujeto %s qu es %s  tiene una edad de: %.1f años\n', SU_g{nS},GR, edad);
+
+
+fprintf(archivo, '%s,%s,%.10f\n', SU_g{nS},GR, edad);
+
+end
+end 
+
+fclose(archivo);
+
 
